10971c18e1176771933565707594dbe7b7c67892
Matheus Alves <matheus.alves@meliuz.com.br>
2021-01-31 17:26:37 -0400

    update: removes required from output option

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ export default () => {
   program
     .version("0.0.1")
     .option("-f --folder <folder>", "A SVGs folder path")
-    .requiredOption("-o --output <output>", "A file to be outputted")
+    .option("-o --output <output>", "A file to be outputted")
     .parse(process.argv);
 
   const options = program.opts();
