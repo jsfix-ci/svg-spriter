b75c809bebf7e23a6ca304dbef460e3c4e384be1
Matheus Alves <matheus.alves@meliuz.com.br>
2021-01-31 17:34:58 -0400

    update: add param and action for config loader in main script

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import fs from 'fs';
 import program from "commander";
 
+import configLoader from './loader/configLoader';
 import folderLoader from './loader/folder';
 
 export default () => {
   program
     .version("0.0.1")
+    .option("-c --config <config>", "A config file")
     .option("-f --folder <folder>", "A SVGs folder path")
     .option("-o --output <output>", "A file to be outputted")
     .parse(process.argv);
@@ -13,12 +15,20 @@ export default () => {
   const options = program.opts();
 
   let content = '';
+  let outputFile = options.output || '';
 
-  if (!!options.folder) {
-    content = folderLoader(options.folder, options.output);
+  /** Should load based config file */
+  if (!!options.config) {
+    const config = configLoader(options.config);
+    content = config.content;
+    outputFile = config.output;
   }
 
-  fs.writeFile(options.output, content, function (err) {
+  if (!options.config && !!options.folder) {
+    content = folderLoader(options.folder);
+  }
+
+  fs.writeFile(outputFile, content, function (err) {
     if (err) {
       console.error(err);
     } else {
